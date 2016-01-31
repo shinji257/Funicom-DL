db39c02666f8b32b24062c25be9acdb5b38864f9
火災。- Kasai <KasaiDot@users.noreply.github.com>
2016-01-31 01:36:53 +0100

    2016-01-15

--- Funicom-DL.bat
+++ Funicom-DL.bat
@@ -1,3 +1,4 @@
+
 @echo off
 cd /D %~dp0
 if exist "Funicom-DL.que.que" del Funicom-DL.que.que
@@ -9,9 +10,9 @@ goto :EOF
 :pro
 if "%~1"=="#" goto :EOF
 if "%~1"=="--quit" del Funicom-DL.que.que
-if "%~1"=="--quit" goto :EOF
+if "%~1"=="--quit" goto exit
 if "%~1"=="-Q" del Funicom-DL.que.que
-if "%~1"=="-Q" goto :EOF
+if "%~1"=="-Q" exit
 if "%~1"=="--global" set G=%*
 if "%~1"=="--global" set G=%G:~2%
 if "%~1"=="--global" goto :EOF
@@ -20,15 +21,15 @@ if "%~1"=="-G" set G=%G:~2%
 if "%~1"=="-G" goto :EOF
 if not exist "tmp" MD "tmp"
 CD tmp
-if exist "..\Funicom-DL.py" copy "..\Funicom-DL.py" "Funicom-DL.py"
 if exist "..\Funicom-DL.pyc" copy "..\Funicom-DL.pyc" "Funicom-DL.pyc"
+if exist "..\Funicom-DL.py" copy "..\Funicom-DL.py" "Funicom-DL.py"
 echo %*>cur.que
 if exist "..\UserExcept.pyni" copy "..\UserExcept.pyni" UserExcept.py
 if not exist "..\UserExcept.pyni" if exist "..\UserExcept.py.ini" copy "..\UserExcept.py.ini" UserExcept.py
-if exist "..\PikaExcept.pyni" copy "..\PikaExcept.pyni" PikaExcept.py
-if not exist "..\PikaExcept.pyni" if exist "..\PikaExcept.py.ini" copy "..\PikaExcept.py.ini" PikaExcept.py
-if exist "..\Funicom-DL.py" Funicom-DL.py %* %g%
-if not exist "..\Funicom-DL.py" if exist "..\Funicom-DL.pyc" Funicom-DL.pyc %* %g%
+if exist "..\PikaExcept.pycni" copy "..\PikaExcept.pycni" PikaExcept.pyc
+if not exist "..\PikaExcept.pycni" if exist "..\PikaExcept.pyni" copy "..\PikaExcept.pyni" PikaExcept.py
+if exist "..\Funicom-DL.pyc" Funicom-DL.pyc %* %g%
+if not exist "..\Funicom-DL.pyc" if exist "..\Funicom-DL.py" Funicom-DL.py %* %g%
 del cur.que
 cd ..
 RD /S /Q tmp
--- Funicom-DL.py
+++ Funicom-DL.py
@@ -2,7 +2,7 @@
 # -*- ver: 2.7 -*-
 # -*- pro: x32 -*-
 
-import cookielib,os,re,sys,urllib,urllib2,fileinput,codecs
+import cookielib,os,re,sys,urllib,urllib2,fileinput,codecs,HTMLParser,time
 from ConfigParser import ConfigParser
 from urlparse import urlparse
 from collections import deque
@@ -18,6 +18,11 @@ try:
 except:
 	PikaExcept_test = False
 None
+def nullInfo(type,type2,data,data2,data3):
+	if type == type2:
+		return str(data+data3)
+	else:
+		return str(data+data2+data3)
 def Exceptions(inAR,inAA,inAL,inSH,inTI,inSN,inEN,inNN,BSNTI):
 	outAR = inAR
 	outAA = inAA
@@ -38,42 +43,103 @@ def Exceptions(inAR,inAA,inAL,inSH,inTI,inSN,inEN,inNN,BSNTI):
 		outAR,outAA,outAL,outSH,outTI,outSN,outEN,outNN,outARs,outAAs,outALs,outSHs = UserExceptions(outAR,outAA,outAL,outSH,outTI,outSN,outEN,outNN,outARs,outAAs,outALs,outSHs,BSNTI)
 	# ----------------------------------------------------------------------
 	return outAR,outAA,outAL,outSH,outTI,outSN,outEN,outNN,outARs,outAAs,outALs,outSHs
-def filedl(dlUrl,authToken,cmd_qq):
-	if cmd_fs == False:
-		ggg = dlUrl.split('/')[-1]
-		ggg = ggg.replace('-480-','-'+cmd_qq+'-')
-		dlUrl = dlUrl+'/'+ggg
-		u = urllib2.urlopen(dlUrl+authToken)
-		file_name = dlUrl.split('/')[-1]
-		f = open(file_name, 'wb')
-		meta = u.info()
-		if cmd_dm == True:
-			print str(meta)
-		file_size = int(meta.getheaders("Content-Length")[0])
-	else:
-		u = urllib2.urlopen(dlUrl+authToken)
-		file_name = dlUrl.split('/')[-1]
-		f = open(file_name, 'wb')
-		meta = u.info()
-		if cmd_dm == True:
-			print str(meta)
-		file_size = int(meta.getheaders("Content-Length")[0])
-	print "Downloading: %s Bytes: %s" % (file_name, file_size)
-
-	file_size_dl = 0
-	block_sz = 8192
+def filedl (dlUrl,authToken,cmd_qq,o_pth,o_fn):
+	file_size1 = 0
+	file_size2 = 0
+	tmpA_o_pth = '\"'+o_pth+'\"'
+	tmpB_o_pth = ''
+	firstrun = True
 	while True:
-		buffer = u.read(block_sz)
-		if not buffer:
-			break
-
-		file_size_dl += len(buffer)
-		f.write(buffer)
-		status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-		status = status + chr(8)*(len(status)+1)
-		print status,
-
-	f.close()
+		if tmpA_o_pth == '':
+			break;
+		else:
+			if firstrun == False:
+				tmpB_o_pth = tmpB_o_pth+'\n               '
+			else:
+				firstrun = False
+			tmpB_o_pth = tmpB_o_pth+tmpA_o_pth[:64]
+			tmpA_o_pth = tmpA_o_pth[64:]
+	o_pth = tmpB_o_pth
+	tmpA_o_fn = '\"'+o_fn+'.mp4\"'
+	tmpB_o_fn = ''
+	firstrun = True
+	while True:
+		if tmpA_o_fn == '':
+			break;
+		else:
+			if firstrun == False:
+				tmpB_o_fn = tmpB_o_fn+'\n               '
+			else:
+				firstrun = False
+			tmpB_o_fn = tmpB_o_fn+tmpA_o_fn[:64]
+			tmpA_o_fn = tmpA_o_fn[64:]
+	o_fn = tmpB_o_fn
+	y = 0-1
+	x = str(int(time.time()))
+	check = False
+	trynum = ''
+	ggg = dlUrl.split('/')[-1]
+	ggg = ggg.replace('-480-','-'+cmd_qq+'-')
+	print 'Downloading File: \n    Time     = '+x+'\n    Name     = '+ggg+'\n    Out Path = '+o_pth+'\n    Out Name = '+o_fn+'\n =============================================================================='
+	try:
+		while True:
+			if check == False:
+				y = y+1
+				if y > 0:
+					trynum = '.try_'+str(y)
+					print 'Try '+str(y)+':'
+				dlUrl2 = dlUrl+'/'+ggg+'.time_'+x+trynum+'.mp4'
+			else:
+				dlUrl2 = dlUrl+'/'+ggg+'.time_'+x+trynum+'.checked.mp4'
+			u = urllib2.urlopen(dlUrl2+authToken)
+			file_name = dlUrl2.split('/')[-1]
+			f = open(file_name, 'wb')
+			meta = u.info()
+			if cmd_dm == True:
+				print str(meta)
+			file_size2 = file_size1
+			file_size1 = int(meta.getheaders("Content-Length")[0])
+			if str(file_size1) == 0:
+				break;
+			elif file_size1 == file_size2:
+				if check == True:
+					print '            '+str(file_size1)+' == '+str(file_size2)+' Bytes'
+					break
+				else:
+					u.close()
+					print '    '+str(file_size1)+' == '+str(file_size2)+' Bytes'
+					check = True
+					print '        Double Checking:'
+			else:
+				check = False
+				u.close()
+				if y > 0:
+					print '    '+str(file_size1)+' != '+str(file_size2)+' Bytes'
+			if (y > 0) and (check != True):
+				time.sleep(30)
+			if check == True:
+				time.sleep(2)
+		file_size = file_size1
+		print "Downloading: %s\n    Bytes: %s" % (file_name, file_size)
+		file_size_dl = 0
+		block_sz = 8192
+		while True:
+			buffer = u.read(block_sz)
+			if not buffer:
+				break
+			file_size_dl += len(buffer)
+			f.write(buffer)
+			status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+			status = status + chr(8)*(len(status)+1)
+			print status,
+		f.close()
+	except urllib2.HTTPError, err:
+		file_name = 'HTTP Error: '+str(err.code)
+		if err.code == 404:
+			file_name = file_name+' File \"'+ggg+'\" Not Found'
+		error(page_url,file_name)
+	# else:
+		# raise
 	return file_name
 def updateque():
 	fileHandle = open ( 'cur.que', 'r' )
@@ -86,6 +152,7 @@ def updateque():
 	os.system('type Funicom-DL.que.tmp>..\\Funicom-DL.que')
 	os.system('del Funicom-DL.que.tmp')
 	sys.exit()
+	return
 def login2(username,password):
 	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie_jar))
 	opener.addheaders =[('Referer', 'http://www.funimation.com/login'),
@@ -111,11 +178,9 @@ def login ():
 	url = 'http://www.funimation.com/profile'
 	req = opener.open(url)
 	site = req.read()
-
 	if re.search(username2+'(?i)',site):
 		print 'Login successful.'
 		cookie_jar.save()
-
 		for line in fileinput.input('cookies.txt',inplace =1):
 			line = line.strip()
 			if not 'c_visitor' in line:
@@ -142,16 +207,22 @@ def config ():
 	#cmdlang = config.get('Funicom-DL', 'lang')
 	#cmddef = config.get('Funicom-DL', 'def')
 def error (url,err):
+	url = re.sub('^http://w?w?w?\.?funimation.com/shows/([^/]+)/.+/([^/]+)$','funi.com/\g<1>/\g<2>',url)
 	try:
 		fileHandle = open ( '..\\error.log', 'r' )
 		errlog = fileHandle.read()
 		fileHandle.close()
 	except IOError:
 		errlog = ''
-	errlog = errlog+url+' - '+err+'\n'
+	errlog = errlog+url+'\n    '+err+'\n'
 	fileHandle = open ( '..\\error.log', 'w' )
 	fileHandle.write(errlog)
 	fileHandle.close()
+	if cmd_dw == True:
+		print 'Error:\n    '+url+'\n        '+err+'\n'
+	if cmd_dp == True:
+		os.system('pause')
+	return
 def getHTML (url):
 	urlparse(url)
 	# try:
@@ -168,7 +239,7 @@ def getHTML (url):
 def cmdargsres ():
 	exec("cmd_l = None") in globals()
 	exec("cmd_q = \'best\'") in globals()
-	exec("cmd_B = None") in globals()
+	exec("cmd_b = None") in globals()
 	exec("cmd_fb = False") in globals()
 	exec("cmd_io = False") in globals()
 	exec("cmd_fq = False") in globals()
@@ -184,6 +255,8 @@ def cmdargsres ():
 	exec("cmd_dc = False") in globals()
 	exec("cmd_dp = False") in globals()
 	exec("cmd_rl = False") in globals()
+	exec("cmd_rl2 = False") in globals()
+	return
 def cmdargs ():
 	cmdargsres()
 	argque = ''
@@ -244,7 +317,8 @@ def cmdargs ():
 				error(page_url,'-q 2nd parameter missing or Not valid')
 				sys.exit()
 			exec(arg+" = \'"+arg2+"\'") in globals()
-		elif arg == 'cmd_B':
+		elif arg.lower() == 'cmd_b':
+			arg = arg.lower()
 			arg2 = argque.popleft()
 			arg2 = re.sub('K','k',arg2)
 			arg2 = re.sub('k','',arg2)
@@ -261,9 +335,10 @@ def cmdargs ():
 			exec("cmd_dd = True") in globals()
 			exec("cmd_dm = True") in globals()
 			exec("cmd_dc = True") in globals()
-			exec("cmd_dp = True") in globals()
+			exec("cmd_dp = True") in globals()			
 		else:
 			exec(arg + " = True") in globals()
+	return
 def uStr (aStr):
 	if cmd_du == True:
 		print aStr
@@ -284,8 +359,19 @@ def prohtml ():
 	playersData = re.findall('.*var playersData = (.*);\n',html).pop()
 	titleData = re.findall('meta property=\"og:title\" content=\"(.*)\">\n',html).pop()
 	titleData = titleData.decode('utf-8')
-	descData = re.findall('meta property=\"og:description\" content=\"(.*)\">\n',html).pop()
+	hp = HTMLParser.HTMLParser()
+	titleData = hp.unescape(titleData)
+#	descData = re.findall('meta property=\"og:description\" content=\"(.*)\">\n',html).pop()
+	htmltmp = re.sub('\n *','',html)
+	htmltmp = re.sub('<p class="date-espires.*','',htmltmp)
+	descData = re.findall('div class=\"showhide full-description[^>]*\">[^<]*<p class=\"lh16\">(.*)</p>',htmltmp).pop()
+	descData = descData.replace('<br />','\n')
+	descData = re.sub('<[^>]*>','',descData)
+	descData = descData.strip()
 	descData = descData.decode('utf-8')
+#	descData = descData.replace('  ','\n')
+	hp = HTMLParser.HTMLParser()
+	descData = hp.unescape(descData)
 	playersData = re.sub('true','True',playersData)
 	playersData = re.sub('false','False',playersData)
 	playersData = re.sub('null','None',playersData)
@@ -320,24 +406,35 @@ def prohtml ():
 	lis_videoSet = dic_epiosdeDataCurrent['videoSet']
 	dic_dubSet = None
 	dic_subSet = None
+	badid = False
 	for iii in lis_videoSet:
 		tmpA = iii
 		try:
 			if tmpA['languageMode'] == 'dub':
 				dic_dubSet = tmpA
-				if cmd_fl == True:
+				if (cmd_fl == True) and (cmd_l == 'jpn'):
 					dic_subSet = tmpA
 			elif tmpA['languageMode'] == 'sub':
 				dic_subSet = tmpA
-				if cmd_fl == True:
+				if (cmd_fl == True) and (cmd_l == 'eng') and (dic_dubSet == None):
 					dic_dubSet = tmpA
+					badid = True
+			if tmpA['languageMode'] == 'dub':
+				if (cmd_fl == True) and (cmd_l == 'jpn') and (dic_subSet == None):
+					dic_subSet = tmpA
+					badid = True
 		except KeyError:
-			x = None
-	BADSeasonTitle = False
+			None
+	None
+	typeExtras = False
+	typeMovie = False
+	BADSeasonTitle = ''
 	if dic_seasonData['title'] == 'Movie':
 		int_Snum = 0
+		typeMovie = True
 	elif dic_seasonData['title'] == 'Extras':
 		int_Snum = 0
+		typeExtras = True
 	elif re.sub('(.+) \d+','\g<1>',dic_seasonData['title']) == 'Season':
 		int_Snum = int(re.findall('\d+',dic_seasonData['title']).pop())
 	else:
@@ -361,6 +458,8 @@ def prohtml ():
 			url_hd1080Url = re.sub('-480-.*m3u8','-480-4000K.mp4',re.sub('/038C48/','/008C48/',dic_dubSet['hd1080Url'].replace('\/','/')))
 			str_FUNImationID = dic_dubSet['FUNImationID']
 			videoId = dic_dubSet['videoId']
+			if badid == True:
+				videoId = ''
 			if cmd_fl == True:
 				str_FUNImationID = str_FUNImationID.replace('JPN','ENG')
 			if url_sdUrl != '':
@@ -402,6 +501,8 @@ def prohtml ():
 			url_hd1080Url = re.sub('-480-.*m3u8','-480-4000K.mp4',re.sub('/038C48/','/008C48/',dic_subSet['hd1080Url'].replace('\/','/')))
 			str_FUNImationID = dic_subSet['FUNImationID']
 			videoId = dic_subSet['videoId']
+			if badid == True:
+				videoId = ''
 			if cmd_fl == True:
 				str_FUNImationID = str_FUNImationID.replace('ENG','JPN')
 			if url_sdUrl != '':
@@ -421,9 +522,9 @@ def prohtml ():
 				if url_hd1080Url == '':
 					url_hd1080Url = 'http://wpc.8c48.edgecastcdn.net/008C48/SV/480/'+str_FUNImationID+'/'+str_FUNImationID+'-480-4000K.mp4'
 			try:
-				str_tmp_Nnum = dic_subSet['number']
-			except KeyError:
 				str_tmp_Nnum = dic_subSet['videoNumber']
+			except KeyError:
+				str_tmp_Nnum = dic_subSet['number']
 			if str_tmp_Nnum == '':
 				str_tmp_Nnum = '0.0'
 			str_Nnum_dec = ('.'+re.sub('\d+\.(\d+)','\g<1>',str_tmp_Nnum))
@@ -480,15 +581,21 @@ def prohtml ():
 		else:
 			error(page_url,'No SD')
 			sys.exit()
-	if cmd_B != None:
-		dlUrl = re.sub('\d\d00K',cmd_B+'K',dlUrl)
+	if cmd_b != None:
+		dlUrl = re.sub('\d\d00K',cmd_b+'K',dlUrl)
+		if cmd_q == '720' and cmd_b <= '2000':
+			# dlUrl = re.sub('SV/480/','SV/720/',dlUrl)
+			dlUrl = re.sub('-480-\d\d00K','-720-'+cmd_b+'K',dlUrl)
+	tag_artist = uStr(tag_artist)
 	tag_artist = uStr(tag_artist)
 	tag_albumartist = uStr(tag_albumartist)
+	BADSeasonTitle = uStr(BADSeasonTitle)
 	tag_album = uStr(tag_album)
 	# tag_title = uStr(tag_title)
-	# tag_description = uStr(tag_description)
+	tag_description = tag_description.replace('\\\'','\'').replace('\\\"','\"')
 	tag_tvshow = tag_albumartist
 	tag_artist,tag_albumartist,tag_album,tag_tvshow,tag_title,int_Snum,int_Enum,int_Nnum,tag_artistsort,tag_albumartistsort,tag_albumsort,tag_tvshowsort = Exceptions(tag_artist,tag_albumartist,tag_album,tag_tvshow,tag_title,int_Snum,int_Enum,int_Nnum,BADSeasonTitle)
+	re.sub('^'+tag_artist+' - ','',titleData)
 	tag_Enum = "{0:0>2}".format(int_Enum)
 	tag_Nnum = "{0:0>4}".format(int_Nnum)
 	tag_Snum = "{0:0>2}".format(int_Snum)
@@ -498,26 +605,74 @@ def prohtml ():
 	rEn = str(int_Enum)
 	rNn = str(int_Nnum)
 	rSn = str(int_Snum)
+	cmdtitlelang = ''
+	if typeExtras == True:
+		cmdtitlelang = 'Extras'
+	elif cmd_l == 'eng':
+		cmdtitlelang = 'Dub'
+	else:
+		cmdtitlelang = 'Sub'
+	global cmdtitle
+	cmdtitle = tag_tvshow+' Episode '+rNn+' - '+tag_title+' ['+cmd_qq+'p] ['+cmdtitlelang+']'
+	os.system('title Funicom-DL ^| '+cmdtitle.encode('utf-8'))
 	En = tag_Enum
 	Nn = tag_Nnum
 	Sn = tag_Snum
 	Dn = tag_Dnum
 	Dn_ = re.sub('\.','_',Dn)
-	show = re.sub('[^\.a-zA-Z0-9 ]','_',tag_tvshow)
-	show32 = re.sub('^(...............................[^ ]).*','\g<1>',show)
-	show16 = re.sub('^(...............[^ ]).*','\g<1>',show)
-	title = re.sub('[^\.a-zA-Z0-9 ]','_',tag_title)
-	title32 = re.sub('^(...............................[^ ]).*','\g<1>',title)
-	title16 = re.sub('^(...............[^ ]).*','\g<1>',title)
-	album = re.sub('[^\.a-zA-Z0-9 ]','_',tag_album)
-	album32 = re.sub('^(...............................[^ ]).*','\g<1>',album)
-	album16 = re.sub('^(...............[^ ]).*','\g<1>',album)
-	albumartist = re.sub('[^\.a-zA-Z0-9 ]','_',tag_albumartist)
-	albumartist32 = re.sub('^(...............................[^ ]).*','\g<1>',albumartist)
-	albumartist16 = re.sub('^(...............[^ ]).*','\g<1>',albumartist)
-	artist = re.sub('[^\.a-zA-Z0-9 ]','_',tag_artist)
-	artist32 =  re.sub('^(...............................[^ ]).*','\g<1>',artist)
-	artist16 = re.sub('^(...............[^ ]).*','\g<1>',artist)
+	show_test = re.sub('.*\$\$nullInfo\$\#.+\$\#.+\#\#.*','true',tag_tvshow)
+	artist_test = re.sub('.*\$\$nullInfo\$\#.+\$\#.+\#\#.*','true',tag_artist)
+	album_test = re.sub('.*\$\$nullInfo\$\#.+\$\#.+\#\#.*','true',tag_album)
+	albumartist_test = re.sub('.*\$\$nullInfo\$\#.+\$\#.+\#\#.*','true',tag_albumartist)
+	title_test = re.sub('.*\$\$nullInfo\$\#.+\$\#.+\#\#.*','true',tag_title)
+	
+	show = re.sub('(.*)\$\$nullInfo\$\#(.+)\$\#(.+)\#\#(.*)','nullInfo(\'\g<2>\',\'tagpars\',\'\g<1>\',\'\g<3>\',\'\g<4>\')',tag_tvshow)
+	artist = re.sub('(.*)\$\$nullInfo\$\#(.+)\$\#(.+)\#\#(.*)','nullInfo(\'\g<2>\',\'tagpars\',\'\g<1>\',\'\g<3>\',\'\g<4>\')',tag_artist)
+	albumartist = re.sub('(.*)\$\$nullInfo\$\#(.+)\$\#(.+)\#\#(.*)','nullInfo(\'\g<2>\',\'tagpars\',\'\g<1>\',\'\g<3>\',\'\g<4>\')',tag_albumartist)
+	album = re.sub('(.*)\$\$nullInfo\$\#(.+)\$\#(.+)\#\#(.*)','nullInfo(\'\g<2>\',\'tagpars\',\'\g<1>\',\'\g<3>\',\'\g<4>\')',tag_album)
+	title = re.sub('(.*)\$\$nullInfo\$\#(.+)\$\#(.+)\#\#(.*)','nullInfo(\'\g<2>\',\'tagpars\',\'\g<1>\',\'\g<3>\',\'\g<4>\')',tag_title)
+	if show_test == 'true':
+		exec("show = "+show)
+	if artist_test == 'true':
+		exec("artist = "+artist)
+	if albumartist_test == 'true':
+		exec("albumartist = "+albumartist)
+	if album_test == 'true':
+		exec("album = "+album)
+	if title_test == 'true':
+		exec("title = "+title)
+	
+	tag_tvshow = re.sub('(.*)\$\$nullInfo\$\#(.+)\$\#(.+)\#\#(.*)','nullInfo(\'\g<2>\',\'tagfiles\',\'\g<1>\',\'\g<3>\',\'\g<4>\')',tag_tvshow)
+	tag_artist = re.sub('(.*)\$\$nullInfo\$\#(.+)\$\#(.+)\#\#(.*)','nullInfo(\'\g<2>\',\'tagfiles\',\'\g<1>\',\'\g<3>\',\'\g<4>\')',tag_artist)
+	tag_albumartist = re.sub('(.*)\$\$nullInfo\$\#(.+)\$\#(.+)\#\#(.*)','nullInfo(\'\g<2>\',\'tagfiles\',\'\g<1>\',\'\g<3>\',\'\g<4>\')',tag_albumartist)
+	tag_album = re.sub('(.*)\$\$nullInfo\$\#(.+)\$\#(.+)\#\#(.*)','nullInfo(\'\g<2>\',\'tagfiles\',\'\g<1>\',\'\g<3>\',\'\g<4>\')',tag_album)
+	tag_title = re.sub('(.*)\$\$nullInfo\$\#(.+)\$\#(.+)\#\#(.*)','nullInfo(\'\g<2>\',\'tagfiles\',\'\g<1>\',\'\g<3>\',\'\g<4>\')',tag_title)
+	if show_test == 'true':
+		exec("tag_tvshow = "+tag_tvshow)
+	if artist_test == 'true':
+		exec("tag_artist = "+tag_artist)
+	if albumartist_test == 'true':
+		exec("tag_albumartist = "+tag_albumartist)
+	if album_test == 'true':
+		exec("tag_album = "+tag_album)
+	if title_test == 'true':
+		exec("tag_title = "+tag_title)
+	
+	show = re.sub('[^\.a-zA-Z0-9 ]','_',show)
+	show32 = show[:32].strip()
+	show16 = show[:16].strip()
+	title = re.sub('[^\.a-zA-Z0-9 ]','_',title)
+	title32 = title[:32].strip()
+	title16 = title[:16].strip()
+	album = re.sub('[^\.a-zA-Z0-9 ]','_',album)
+	album32 = album[:32].strip()
+	album16 = album[:16].strip()
+	albumartist = re.sub('[^\.a-zA-Z0-9 ]','_',albumartist)
+	albumartist32 = albumartist[:32].strip()
+	albumartist16 = albumartist[:16].strip()
+	artist = re.sub('[^\.a-zA-Z0-9 ]','_',artist)
+	artist32 = artist[:32].strip()
+	artist16 = artist[:16].strip()
 	exec("outFilename = " + nameformat)
 	exec("outpath = " + pathformat)
 	fileHandle = open ( str(int_Enum)+Dn_+'.funi.info', 'w' )
@@ -587,37 +742,32 @@ def prohtml ():
 	if cmd_io == True:
 		updateque()
 	if cmd_fq == True:
-		try:
-			file_name = filedl(dlUrl,authToken,cmd_qq)
-		except urllib2.HTTPError:
-			try:
-				dlUrl = url_hdUrl
-				highy = ''
-				defy = 'HD'
-				exec("outFilename = " + nameformat)
-				file_name = filedl(dlUrl,authToken,cmd_qq)
-			except urllib2.HTTPError:
-				try:
-					highy = ''
-					defy = 'SD'
-					exec("outFilename = " + nameformat)
-					dlUrl = url_sdUrl
-					file_name = filedl(dlUrl,authToken,cmd_qq)
-				except urllib2.HTTPError:
-					error(page_url,'fail to force quality')
-					sys.exit()
+		file_name = filedl(dlUrl,authToken,cmd_qq,outpath,outFilename)
+		if re.sub('HTTP Error: .+','HTTP Error: True',file_name) != 'HTTP Error: True':
+			error(page_url,'fail to force quality')
+			sys.exit()
 	else:
-		file_name = filedl(dlUrl,authToken,cmd_qq)
+		file_name = filedl(dlUrl,authToken,cmd_qq,outpath,outFilename)
 	s = 0
-	# if cmd_rl == True:
-	# 	if defy == '1080p':
-	# 		os.system('rl.bat \"'+file_name+'\"')
-	cmd = 'move \"'+file_name+'\" \"'+outpath+outFilename+'.mp4\"'
-	os.system(cmd)
+	if re.sub('HTTP Error: .+','HTTP Error: True',file_name) != 'HTTP Error: True':
+		if cmd_rl == True:
+			if defy == '1080p':
+				os.system('rl.bat \"'+file_name+'\"')
+		if cmd_rl2 == True:
+			if defy == '1080p':
+				os.system('rl.bat \"'+file_name+'\" keep')
+				cmd = 'move \"'+file_name+'.mp4\" \"'+outpath+outFilename+'.mp4.mp4\"'
+				os.system(cmd)
+		cmd = 'move \"'+file_name+'\" \"'+outpath+outFilename+'.mp4\"'
+		print "cmd = 'move \"'"+file_name+"'\" \"'"+outpath+outFilename+"'.mp4\"'"
+		os.system(cmd)
 	if cmd_dc == False:
 		os.system('cls')
 	if cmd_dp == True:
 		os.system('pause')
+	if re.sub('HTTP Error: .+','HTTP Error: True',file_name) != 'HTTP Error: True':
+		updateque()
+	sys.exit()
 #----------
 print 'Booting up...'
 
@@ -626,5 +776,5 @@ config()
 #	login()
 cmdargs()
 prohtml()
-updateque()
+
 #----------
